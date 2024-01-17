d7a66f45149c18ccc2aed053406d18f20601901d
ChristianBraunagel <62561756+ChristianBraunagel@users.noreply.github.com>
2024-01-17 21:08:39 +0100

    add user input and basic input check

--- main.py
+++ main.py
@@ -2,5 +2,18 @@
 exit = bool(False)
 
 while(not exit):
-    print(exit)
-    exit = True
+
+    # ask for user input
+    triangular_num = input("Which triangular number should be visualized? Type 'q' to quit.\n")
+
+    # user wants to exit
+    if triangular_num == 'q':
+        exit = True
+        continue
+
+    # basic checks for input
+    if not triangular_num.isnumeric():
+        print("Your input is not a positive number.")
+        continue
+
+
