38effeab50383bce0587cdc48c81a86e50c5d87c
ChristianBraunagel <62561756+ChristianBraunagel@users.noreply.github.com>
2024-01-17 21:45:02 +0100

    add first implementation of a test for triangular number

---
+++ triangular_math.py
@@ -0,0 +1,16 @@
+def isTriangularNumber(num):
+    # method 1 using a loop
+    # starting at 1 and calculates always the next triangular number
+    # until the result is equal or larger than the number under test
+
+    result = int(0)
+    iterator = int(1)
+
+    while result<num:
+        result = result + iterator
+        iterator += 1
+    
+    if result == num:
+        return True
+    else:
+        return False
